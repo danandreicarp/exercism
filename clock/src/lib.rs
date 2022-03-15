33471dd1e2cc2181e75471b0b6fb92d5a5923e46
danandreicarp <carpdan@pm.me>
2022-03-15 22:55:10 +0200

    clock v2

--- clock/src/lib.rs
+++ clock/src/lib.rs
@@ -1,7 +1,4 @@
-use std::{
-    fmt::Display,
-    ops::{Div, Rem},
-};
+use std::fmt::Display;
 
 const MINS_IN_HOUR: i32 = 60;
 const HOURS_IN_DAY: i32 = 24;
@@ -15,24 +12,13 @@ pub struct Clock {
 impl Clock {
     pub fn new(hours: i32, minutes: i32) -> Self {
         // transform minutes into hours
-        let mut total_hours = hours + minutes.div(MINS_IN_HOUR);
+        let total_hours = hours + minutes.div_euclid(MINS_IN_HOUR);
 
         // keep remaining minutes
-        let mut minutes = minutes.rem(MINS_IN_HOUR);
-
-        // handle negative minutes e.g. -15 -> 45
-        if minutes < 0 {
-            minutes += MINS_IN_HOUR;
-            total_hours -= 1;
-        }
+        let minutes = minutes.rem_euclid(MINS_IN_HOUR);
 
         // reduce hours to valid value
-        let mut hours = total_hours.rem(HOURS_IN_DAY);
-
-        // handle negative hours e.g. -1 -> 23
-        if hours < 0 {
-            hours += HOURS_IN_DAY;
-        }
+        let hours = total_hours.rem_euclid(HOURS_IN_DAY);
 
         Clock {
             hours: hours as u8,
@@ -41,18 +27,14 @@ impl Clock {
     }
 
     pub fn add_minutes(&self, minutes: i32) -> Self {
-        let hours = minutes.div(MINS_IN_HOUR);
-        let minutes = minutes.rem(MINS_IN_HOUR);
+        let hours = minutes.div_euclid(MINS_IN_HOUR);
+        let minutes = minutes.rem_euclid(MINS_IN_HOUR);
 
         let total_minutes = minutes + self.minutes as i32;
         let total_hours = hours + self.hours as i32;
 
         Clock::new(total_hours, total_minutes)
     }
-
-    // pub fn to_string(&self) -> String {
-    //     format!("{:02}:{:02}", self.hours, self.minutes)
-    // }
 }
 
 impl Display for Clock {
