35e974960edd30b65a9b8ba1d83523ff1681f78b
danandreicarp <carpdan@pm.me>
2022-04-01 22:32:53 +0300

    luhn checksum v2

--- luhn/src/lib.rs
+++ luhn/src/lib.rs
@@ -9,23 +9,21 @@ pub fn is_valid(code: &str) -> bool {
         let mut should_double = true;
         for ch in code.chars().rev() {
             should_double = !should_double;
-            match ch {
-                '0'..='9' => {
-                    if should_double {
-                        let mut dbl = ch.to_digit(10).unwrap() as u8 * 2;
-                        if dbl > 9 {
-                            dbl -= 9;
-                        }
-                        sum += dbl;
-                    } else {
-                        sum += ch.to_digit(10).unwrap() as u8;
+            match (ch.to_digit(10), should_double) {
+                (Some(d), true) => {
+                    let mut dbl = d as u8 * 2;
+                    if dbl > 9 {
+                        dbl -= 9;
                     }
+                    sum += dbl;
+                }
+                (Some(d), false) => {
+                    sum += d as u8;
                 }
                 _ => {
                     return false;
                 }
             }
-            println!("{}", sum);
         }
 
         sum % 10 == 0
